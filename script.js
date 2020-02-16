766e05dc3c8416d2d8debffad863def2d760d0ed
u0899341 <u0899341@server4.hosting.reg.ru>
2020-02-16 18:32:09 +0300

    final3

--- script.js
+++ script.js
@@ -41,24 +41,25 @@ $(function() {
         $(".header__burger").show();
       });
   });
-  $(".show-call-request").click(function(event) {
-    event.preventDefault();
-    $("body").toggleClass("scroll-lock");
-    $(".popup-container").show();
-    $(".form__input_email").hide();
-  });
-  $(".show-call-request-email").click(function(event) {
-    event.preventDefault();
-    $("body").toggleClass("scroll-lock");
-    $(".popup-container").show();
-    $(".form__input_email").show();
-  });
-  $(".popup-container").click(function(event) {
+    $(".show-call-request").click(function(event){
+        var form1 = $('<form class="form" action="https://echo.htmlacademy.ru/"><label class="form__input"><span>Пожалуйста,<br>укажите Ваше имя</span><input type="text" required="required" name="name"></label><label class="form__input"><span>номер телефона</span><input type="tel" required="required" name="phone" placeholder="+7 (___) ___-__-__"></label><input class="send" type="submit" name="submit" value="Отправить"></form>');
+        $("body").toggleClass("scroll-lock");
+        $(".popup-container").show();
+        $(".popup").append(form1);
+    });
+    $(".show-call-request-email").click(function(event) {
+        var form2 = $('<form class="form" action="https://echo.htmlacademy.ru/"><label class="form__input"><span>Пожалуйста,<br>укажите Ваше имя</span><input type="text" required="required" name="name"></label><label class="form__input"><span>номер телефона</span><input type="tel" required="required" name="phone" placeholder="+7 (___) ___-__-__"></label><label class="form__input form__input_email">	<span>адрес электронный почты</span><input type="email" required="required" name="email"></label><input class="send" type="submit" name="submit" value="Отправить"></form>');
+        $("body").toggleClass("scroll-lock");
+        $(".popup-container").show();
+        $(".popup").append(form2);
+    });
+    $(".popup-container").click(function(event) {
     if (event.target == this) {
+      $('.popup').find(".form").remove();
       $(this).hide();
       $("body").toggleClass("scroll-lock");
-    }
-  });
+    };
+    });
 });
 $(document).ready(function() {
   $('input[type="tel"]').inputmask({ mask: "+7 (999) 999-99-99" });
